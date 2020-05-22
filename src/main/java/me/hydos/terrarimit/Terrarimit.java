557e90a16ed9b864b12e109507a6ebf214fe234f
hYdos <haydenv06@gmail.com>
2020-05-22 14:01:55 +1000

    yes

---
+++ src/main/java/me/hydos/terrarimit/Terrarimit.java
@@ -0,0 +1,11 @@
+package me.hydos.terrarimit;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Terrarimit implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+		System.out.println("Hello Fabric world!");
+	}
+}
